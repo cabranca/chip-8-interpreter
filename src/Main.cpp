3d9afb5f45354ced96757199932db90bcebd9baf
cabranca <joaquincabrera98@gmail.com>
2026-02-20 18:09:40 -0300

    Test SDL inclusion and linking.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,12 @@
+#include <SDL3/SDL.h>
 #include <iostream>
+
 
 int main(int argc, char** argv) {
     int a = 0;
     a++;
+
+    bool res = SDL_Init(SDL_INIT_VIDEO);
 
     std::cout << "Hello World!" << a << std::endl;
 
