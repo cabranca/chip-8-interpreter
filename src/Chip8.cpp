1723b52fe41cdbcabad098a8069b655e1ce26ada
cabranca <joaquincabrera98@gmail.com>
2026-02-21 17:23:33 -0300

    Add switch cases with some implementations.

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -1,24 +1,34 @@
 #include "Chip8.h"
 
-namespace chip8 {
+namespace chip8
+{
+    constexpr uint16_t X_MASK = 0x0F00;
+    constexpr uint16_t Y_MASK = 0x00F0;
+    constexpr uint16_t N_MASK = 0x000F;
+    constexpr uint16_t NN_MASK = 0x00FF;
+    constexpr uint16_t NNN_MASK = 0x0FFF;
 
     Chip8::Chip8(SDL_Texture *texture) : m_Texture(texture)
     {
-
+        m_Memory[0x200] = 1;
+        m_Memory[0x201] = 2;
     }
 
     void Chip8::run()
     {
         bool running = true;
-        while (running) {
+        while (running)
+        {
             SDL_Event event;
-            while(SDL_PollEvent(&event)) {
-                switch(event.type){
-                    case SDL_EVENT_QUIT:
-                        running = false;
-                        break;
-                    default:
-                        break;
+            while (SDL_PollEvent(&event))
+            {
+                switch (event.type)
+                {
+                case SDL_EVENT_QUIT:
+                    running = false;
+                    break;
+                default:
+                    break;
                 }
             }
 
@@ -29,34 +39,70 @@ namespace chip8 {
 
     uint16_t Chip8::fetch()
     {
-        uint16_t instruction = m_Memory.at(m_PC++) << 8;
-        instruction |= m_Memory.at(m_PC++);
-        return instruction;
+        const uint16_t high = m_Memory.at(m_PC);
+        const uint16_t low = m_Memory.at(m_PC + 1);
+
+        // Only increment now to ensure the read hasn't thrown exception
+        m_PC += 2;
+        return (high << 8 | low);
     }
 
     void Chip8::decodeExecute(uint16_t instruction)
     {
-        uint8_t opcode = instruction & 15 << 12;
+        const uint8_t opcode = static_cast<uint8_t>(instruction >> 12);
+        uint8_t vx = static_cast<uint8_t>((instruction & X_MASK) >> 8);
+        uint8_t vy = static_cast<uint8_t>((instruction & Y_MASK) >> 4);
+        uint8_t n = static_cast<uint8_t>(instruction & N_MASK);
+        uint8_t nn = static_cast<uint8_t>(instruction & NN_MASK);
+        uint16_t nnn = instruction & NNN_MASK;
         switch (opcode)
         {
-            case 0:
-                // Clear Screen
-                break;
-            case 1:
-                // Jump
-                break;
-            case 6:
-                // Set register Vx
-                break;
-            case 7:
-                // Add value to Vx
-                break;
-            case 0xA:
-                // Set index register I
-                break;
-            case 0xD:
-                // Display/Draw
-                break;
+        case 0:
+            // Clear Screen
+            break;
+        case 1:
+            jump(instruction & NNN_MASK);
+            break;
+        case 6:
+            setRegister(vx, nn);
+            break;
+        case 7:
+            addRegister(vx, nn);
+            break;
+        case 0xA:
+            setIndexRegister(instruction & NNN_MASK);
+            break;
+        case 0xD:
+            draw(vx, vy, n);
+            break;
         }
     }
-}
+
+    void Chip8::clearScreen()
+    {
+    }
+
+    void Chip8::jump(uint16_t address)
+    {
+        m_PC = address;
+    }
+
+    void Chip8::setRegister(uint8_t index, uint8_t value)
+    {
+        m_Reg.at(index) = value;
+    }
+
+    void Chip8::addRegister(uint8_t index, uint8_t value)
+    {
+        m_Reg.at(index) += value;
+    }
+
+    void Chip8::setIndexRegister(uint16_t value)
+    {
+        m_IReg = value;
+    }
+
+    void Chip8::draw(uint8_t regx, uint8_t regy, uint8_t height)
+    {
+    }
+} // namespace chip8
--- src/Chip8.h
+++ src/Chip8.h
@@ -5,31 +5,40 @@
 #include <array>
 #include <cstdint>
 
-namespace chip8 {
+namespace chip8
+{
 
-    class Chip8 {
-        public:
-            Chip8(SDL_Texture* texture);
+    class Chip8
+    {
+      public:
+        Chip8(SDL_Texture *texture);
 
-            void run();
-            uint16_t fetch();
-            void decodeExecute();
+        void run();
 
-        private:
-            uint16_t m_PC = 0x200;
-            
-            std::array<uint8_t, 4096> m_Memory;
-            std::array<uint8_t, 16> m_Reg;
-            uint16_t m_IReg = 0;
-            
-            std::array<uint16_t, 16> m_Stack;
-            uint8_t m_StackPtr = 0;
-            
-            std::array<uint8_t, 64 * 32> m_Video;
-            
-            uint8_t m_Delay = 0;
-            uint8_t m_Sound = 0;
+      private:
+        uint16_t m_PC = 0x200;
 
-            SDL_Texture* m_Texture;
+        std::array<uint8_t, 4096> m_Memory;
+        std::array<uint8_t, 16> m_Reg;
+        uint16_t m_IReg = 0;
+
+        std::array<uint16_t, 16> m_Stack;
+        uint8_t m_StackPtr = 0;
+
+        std::array<uint8_t, 64 * 32> m_Video;
+
+        uint8_t m_Delay = 0;
+        uint8_t m_Sound = 0;
+
+        SDL_Texture *m_Texture;
+
+        uint16_t fetch();
+        void decodeExecute(uint16_t instruction);
+        void clearScreen();
+        void jump(uint16_t address);
+        void setRegister(uint8_t index, uint8_t value);
+        void addRegister(uint8_t index, uint8_t value);
+        void setIndexRegister(uint16_t value);
+        void draw(uint8_t regx, uint8_t regy, uint8_t height);
     };
-}
+} // namespace chip8
