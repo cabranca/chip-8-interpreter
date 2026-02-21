dac413510f9ab1e3feab72b983fb62af648a5dfd
cabranca <joaquincabrera98@gmail.com>
2026-02-21 13:00:51 -0300

    Updated to C++23.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,13 @@
 #include <SDL3/SDL.h>
-#include <iostream>
+#include <print>
 
 
 int main(int argc, char** argv) {
-    int a = 0;
-    a++;
 
     bool res = SDL_Init(SDL_INIT_VIDEO);
 
-    std::cout << "Hello World!" << a << std::endl;
+
+    std::print("{2} {0}{1}", "World", "!", "Hello");
 
     return 0;
 }
